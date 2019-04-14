f88c09f3549c931502c52a35db29c80897781124
salviolorenzoWela <salviolorenzo@gmail.com>
2019-04-14 16:29:13 -0400

    adding basic

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Search from './Search';
 import Login from './Login';
 import shortLogo from './images/s-o-logo.png';
 import lrgLogo from './images/s-o-logo-long.jpg';
+require('dotenv').config();
 
 class App extends Component {
   constructor(props) {
@@ -12,10 +13,16 @@ class App extends Component {
     this.state = {};
   }
 
+  componentDidMount() {
+    console.log(process.env);
+  }
+
   handleLogin(event) {
     event.preventDefault();
     const userName = event.target.username.value;
     const pw = event.target.password.value;
+    console.log(userName);
+    console.log(pw);
   }
 
   render() {
